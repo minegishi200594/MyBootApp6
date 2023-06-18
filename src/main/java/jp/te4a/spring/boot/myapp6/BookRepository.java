66a3112eb433931c257d9e4d810fe73c7b688669
minegishi200594 <tejc200594@gmail.com>
2023-06-19 00:56:40 +0900

    kadai6

---
+++ src/main/java/jp/te4a/spring/boot/myapp6/BookRepository.java
@@ -0,0 +1,27 @@
+package jp.te4a.spring.boot.myapp6;
+
+
+import java.util.List;
+import java.util.ArrayList;
+import org.springframework.stereotype.Repository;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+
+@Repository
+public class BookRepository {
+	private final ConcurrentMap<Integer,BookBean>bookMap = new ConcurrentHashMap<>();
+	
+	public BookBean save(BookBean bookbean) {
+		return bookMap.put(bookbean.getId(), bookbean);
+	}
+	
+	public void delete(Integer bookId) {
+		bookMap.remove(bookId);
+	}
+	
+	public List<BookBean> findAll(){
+		return new ArrayList<>(bookMap.values());
+	}
+}
