bcfecc2c175f419f5cbe0b9ff43e8bafe0b44820
minegishi200594 <tejc200594@gmail.com>
2023-07-03 10:14:57 +0900

    kadai10

--- src/main/java/jp/te4a/spring/boot/mybootapp9/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package jp.te4a.spring.boot.mybootapp9;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<BookBean, Integer> {
-}
